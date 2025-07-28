3bd7703711e6ace92a452d8327fdaa020b87a4b7
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2025-07-28 17:29:03 -0300

    Add badge component

---
+++ src/components/CustomBadge.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+type Variant = 'green' | 'yellow' | 'blue'
+
+const props = withDefaults(
+  defineProps<{
+    variant?: Variant
+  }>(),
+  {
+    variant: 'blue',
+  },
+)
+
+const variantClasses: Record<Variant, string> = {
+  green: 'bg-green-200 text-green-800',
+  yellow: 'bg-yellow-200 text-yellow-800',
+  blue: 'bg-blue-200 text-blue-800',
+}
+</script>
+
+<template>
+  <span
+    :class="`inline-flex items-center rounded-full px-3 py-1 text-sm font-medium ${variantClasses[props.variant]}`"
+  >
+    <slot />
+  </span>
+</template>
